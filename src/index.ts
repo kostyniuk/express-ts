6d305a27639773bd5042d48c46f2c37f8423fc8c
Alex Kostyniuk <kostyniuk7@gmail.com>
2026-01-03 19:22:49 +0100

    setup oxlint, oxfmt

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Node.js! Updated");
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from Express + tsgo!" });
+});
+
+app.listen(3001, () => {
+  const port = 3001;
+
+  console.log(`Server running on http://localhost:${port}`);
+});
